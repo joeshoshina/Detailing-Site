4f7675a7a0eda787c8b772e221f2c4a53c7061ac
Joe Hoshina <jhoshina@uci.edu>
2025-09-29 21:45:02 -0700

    Setup for routing  for when I add db logic for reviews, currently working on one page setup with anchoring

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import Reviews from "./pages/Reviews.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} /> {/* landing page */}
+        <Route path="/reviews" element={<Reviews />} /> {/* reviews page */}
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
