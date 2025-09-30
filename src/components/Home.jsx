674f8a5eb8bcad348e648f2e03b7e9734b123e91
Joe Hoshina <jhoshina@uci.edu>
2025-09-30 14:49:51 -0700

    Finished homepage, may alter later depending on client wishes

---
+++ src/components/Home.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import heroImg from "../assets/hero.png";
+
+const Home = () => {
+  const [book, setBook] = useState(false);
+
+  return (
+    <div className="flex flex-col">
+      <section
+        id="home"
+        className="h-150 w-full bg-cover bg-center relative"
+        style={{ backgroundImage: `url(${heroImg})` }}
+      >
+        <div className="absolute inset-0 bg-black/40 flex flex-col items-center justify-center md:px-20 px-6">
+          <h1 className="text-4xl md:text-5xl font-bold text-white text-center pb-3">
+            Premium Mobile Auto Detailing Across the San Fernando Valley — Shine
+            That Lasts”
+          </h1>
+          {/* Book now button */}
+          <button onClick={setBook} className="bg-white px-2 rounded-lg">
+            Book Now!
+          </button>
+          {/* Phone number */}
+        </div>
+      </section>
+      <p className="py-20 text-lg justify-center max-w-2xl mx-auto text-gray-800">
+        Rich’s Mobile Detailing provides professional, convenient car care
+        across the San Fernando Valley. Whether you need a quick exterior wash,
+        a full interior clean, or complete paint correction, we bring
+        top-quality detailing right to your driveway. Our mission is simple —
+        keep your car spotless, shiny, and looking its best, without you ever
+        leaving home.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
