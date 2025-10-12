495ae2d1d2f0c88dc4c14841ade8e761ebae5156
Joe Hoshina <jhoshina@uci.edu>
2025-10-11 21:16:28 -0700

    Finished about, received raw paragraph/idea fixed then made p elements

---
+++ src/components/About.jsx
@@ -0,0 +1,30 @@
+const About = () => {
+  return (
+    <section
+      id="about"
+      className="text-center flex flex-col bg-gray-200 bg-contain py-20"
+    >
+      <h1 className="text-2xl font-bold text-black mb-5">CR Auto Detailing</h1>
+      <p className="px-8 text-lg text-left max-w-2xl mx-auto text-gray-700 ">
+        Hey, we’re Rich and Calum — two CSUN students with a drive to offer
+        purpose. What started as us cleaning our own cars turned into a way to
+        make ends meet. Over time, what began as a simple hustle became a
+        passion we saw real vision in.
+      </p>
+      <p className="px-8 text-lg text-left max-w-2xl mx-auto text-gray-700 mt-3">
+        But we wouldn’t be here without our friends. They were the first to
+        trust us with their cars, the first to spread the word, and even helped
+        bring this site to life. Our growth was built on that support — proof
+        that when your bros believe in you, anything can happen.
+      </p>
+      <p className="px-8 text-lg text-left max-w-2xl mx-auto text-gray-700 mt-3">
+        We’re founded on passion, hard work, and most importantly, the
+        overwhelming support from the people who had our backs since day one. We
+        guarantee you’ll leave not just satisfied, but confident in trusting us
+        for your next clean.
+      </p>
+    </section>
+  );
+};
+
+export default About;
