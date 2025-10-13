bbb8696970a4f246d08446221a9cd5ff65f2d36c
Joe Hoshina <jhoshina@uci.edu>
2025-10-12 20:04:10 -0700

    removed review added booking route

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
-import Reviews from "./pages/Reviews.jsx";
+import Booking from "./pages/Booking.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} /> {/* landing page */}
-        <Route path="/reviews" element={<Reviews />} /> {/* reviews page */}
+        <Route path="/book" element={<Booking />} /> {/* reviews page */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
